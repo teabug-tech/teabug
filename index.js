1c78e3384d7231a579f6525db812841157fcf5c7
Mohamed Haddi <mhaddi@student.1337.ma>
2021-03-18 16:27:36 +0100

    add pixels animation on click

--- index.js
+++ index.js
@@ -1,53 +1,25 @@
 let card = document.getElementById("card");
-const colorArray = ["#000", "#FFFFFF"];
+let cardWidthPx = card.offsetWidth;
+let pixels = Array.from(document.getElementsByClassName("pixel"));
+let pixelEdgePx = Math.floor(cardWidthPx / 29);
+let pixelEdgeVw = (pixelEdgePx * 80) / cardWidthPx;
+const colorArray = ["#ECECEC", "#FFFFFF"];
 
-let pixels = document.getElementsByClassName("pixel");
-
-Array.from(pixels).forEach((e) => {
-    e.style.backgroundColor = colorArray[Math.floor(Math.random() * 2)];
-    let pixelEdgePx = Math.floor(card.offsetWidth / 29);
-    let pixelEdgeVw = (pixelEdgePx * 80) / card.offsetWidth;
-    e.style.width = `${pixelEdgeVw}vw`;
-    e.style.height = `${pixelEdgeVw}vw`;
+pixels.forEach((pixel) => {
+    pixel.style.width = `${pixelEdgeVw}vw`;
+    pixel.style.height = `${pixelEdgeVw}vw`;
+    pixel.style.backgroundColor = colorArray[Math.floor(Math.random() * 2)];
 });
 
-/*
-function pixelate(numSquaresHor, numSquaresVer, pixelWidth, pixelHeight) {
-    card.innerHTML = "";
-    card.style.backgroundColor = "#ffffff";
-    for (let i = 0; i < numSquaresHor * numSquaresVer; i++) {
-        let newPixel = document.createElement("div");
-        newPixel.classList.add("pixel");
-        newPixel.style.width = pixelWidth;
-        newPixel.style.height = pixelHeight;
-        newPixel.style.backgroundColor =
-            colorArray[Math.floor(Math.random() * 2)];
-        newPixel.style.display = "inline-block";
-        newPixel.style.marginBottom = "-4px";
-        card.appendChild(newPixel);
-    }
+function pixelate() {
+    pixels.forEach((pixel) => {
+        pixel.style.backgroundColor = colorArray[Math.floor(Math.random() * 2)];
+    });
 }
 
 function animatePixels() {
-    let pixel = Array.from(document.getElementsByClassName("pixel"))[0];
-    let pixelWidth = pixel.offsetWidth;
-    let pixelHeight = pixel.offsetHeight;
-    let numSquaresHor = Math.floor(card.offsetWidth / pixel.offsetWidth);
-    let numSquaresVer = Math.floor(card.offsetHeight / pixel.offsetHeight);
-    let corners = document.getElementById("corners");
-    for (let i = 0; i < 5; i++) {
-        setTimeout(function () {
-            pixelate(numSquaresHor, numSquaresVer, pixelWidth, pixelHeight);
-            setTimeout(function () {
-                card.innerHTML = "";
-                card.appendChild(corners);
-                card.style.backgroundColor = "#000";
-                Array.from(pixels).forEach((e) => {
-                    e.style.backgroundColor =
-                        colorArray[Math.floor(Math.random() * 2)];
-                });
-            }, 0);
-        }, 5);
-    }
+    let timerId = setInterval(pixelate, 100);
+    setTimeout(() => {
+        clearInterval(timerId);
+    }, 500);
 }
-*/
