165bc72f02ac42418589a134735ed5fd3bd98d3d
Mohamed Haddi <mhaddi@student.1337.ma>
2021-03-18 16:46:47 +0100

    finish animation

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 let card = document.getElementById("card");
 let cardWidthPx = card.offsetWidth;
+let corners = Array.from(document.getElementById("corners").children);
 let pixels = Array.from(document.getElementsByClassName("pixel"));
 let pixelEdgePx = Math.floor(cardWidthPx / 29);
 let pixelEdgeVw = (pixelEdgePx * 80) / cardWidthPx;
 const colorArray = ["#ECECEC", "#FFFFFF"];
+
+function displayOneCorner() {
+    corners.forEach((corner) => {
+        corner.style.display = "none";
+    });
+    let randomCorner = corners[Math.floor(Math.random() * 4)];
+    randomCorner.style.display = "block";
+}
+
+displayOneCorner();
 
 pixels.forEach((pixel) => {
     pixel.style.width = `${pixelEdgeVw}vw`;
@@ -18,8 +29,12 @@ function pixelate() {
 }
 
 function animatePixels() {
+    corners.forEach((corner) => {
+        corner.style.display = "block";
+    });
     let timerId = setInterval(pixelate, 100);
     setTimeout(() => {
+        displayOneCorner();
         clearInterval(timerId);
     }, 500);
 }
