e58aceb75c94001bc6039bdea0db9efe9e72de38
Mohamed Haddi <m.haddi.95@gmail.com>
2021-03-19 23:22:33 +0100

    finish responsiveness

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ let section = document.getElementsByTagName("Section")[0];
 let cardWidthPx = card.offsetWidth;
 let corners = Array.from(document.getElementById("corners").children);
 let pixels = Array.from(document.getElementsByClassName("pixel"));
-// let pixelEdgePx = Math.floor(cardWidthPx / 29);
-// let pixelEdgeVw = (pixelEdgePx * 80) / cardWidthPx;
 const colorArray = ["#ececec", "#ffffff"];
 
 function displayOneCorner() {
@@ -15,15 +13,6 @@ function displayOneCorner() {
     });
     let randomCorner = corners[Math.floor(Math.random() * 4) + 1];
     randomCorner.style.display = "block";
-    /*
-    if (randomCorner.getAttribute("id").includes("top")) {
-        section.style.paddingBottom = "0";
-        section.style.paddingTop = "2.75862vw";
-    } else if (randomCorner.getAttribute("id").includes("bottom")) {
-        section.style.paddingTop = "0";
-        section.style.paddingBottom = "2.75862vw";
-    }
-    */
 }
 
 displayOneCorner();
