7d10b63f9687eed561b60453393c4fe5cc581a03
Mohamed taha Baz <mobaz@e0r4p7.1337.ma>
2021-10-10 13:05:39 +0100

    add routing logic and textual animation

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,8 +5,64 @@ let corners = Array.from(document.getElementById("corners").children);
 let pixels = Array.from(document.getElementsByClassName("pixel"));
 const colorArray = ["#ececec", "#ffffff"];
 
-displayOneCorner();
-pixelate();
+class TextScramble {
+    constructor(el){
+      this.el = el;
+      this.chars = '!<>-_\\/[]{}—=+*^?#________';
+      this.update = this.update.bind(this);
+    }
+    
+    setText(newText){
+      const oldText = this.el.innerText;
+      const length = Math.max(oldText.length,newText.length);
+      const promise = new Promise((resolve)=>this.resolve = resolve);
+      
+      this.queue = [];
+      for(let i = 0;i<length;i++){
+        const from = oldText[i] || '';
+        const to  = newText[i] || '';
+        const start = Math.floor(Math.random()*40);
+        const end = Math.floor(Math.random()*40) + start;
+        this.queue.push({from,to,start,end});
+      }
+      
+      cancelAnimationFrame(this.frameRequest);
+      this.frame = 0;
+      this.update();
+      return promise;
+    }
+    
+    update(){
+      let output = '';
+      let complete = 0;
+      for(let i=0,n=this.queue.length;i<n;i++){
+        let {from,to,start,end,char} = this.queue[i];
+        
+        if(this.frame >= end){
+          complete++;
+          output += to;
+        }else if(this.frame >= start){
+          if(!char || Math.random() < 0.28){char = this.randomChar();this.queue[i].char = char;}
+          output += `<span class='dud'>${char}</span>`;
+        }else {
+          output += from;
+        }
+      }
+      this.el.innerHTML = output;
+      if(complete === this.queue.length){this.resolve();}else{
+        this.frameRequest = requestAnimationFrame(this.update);this.frame++;}
+    }
+    
+    randomChar(){
+      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+  }
+  
+  
+
+document.addEventListener('DOMContentLoaded', function() {
+    animatePixels(document.getElementsByClassName("focused")[0])
+ }, false);
 
 function displayOneCorner() {
     corners.forEach((corner) => {
@@ -24,7 +80,49 @@ function pixelate() {
     });
 }
 
+function getNewContent(name) {
+    switch (name) {
+        case "about":
+            return "About"
+            break;
+        case "services":
+            return "Services"
+            break;
+        case "workflow":
+            return "workflow"
+            break
+        case "contact":
+            return "contact"
+            break
+        case "tech-stack":
+            return "tech-stack"
+            break
+        default:
+            return "lorem ipsum dolor sit amet, consectetur\
+            adipiscing elit. tincidunt blandit imperdiet\
+            auctor congue scelerisque dolor. felis integer\
+            viverra sodales integer arcu donec."
+            break;
+    }
+}
+
+
+
+function changeChars(text, el) {
+    const fx = new TextScramble(el);
+    
+    const next = () => {
+      fx.setText(text)
+    }
+    
+    next()
+}
+
+
 function animatePixels(e) {
+    let pageName = e.name
+    let descriptionEl = document.getElementsByClassName("description-content")[0]
+    window.history.replaceState({}, pageName, pageName+'.html')
     document.getElementsByClassName("focused")[0].classList.remove("focused");
     e.classList.add("focused");
     corners.forEach((corner) => {
@@ -33,8 +131,9 @@ function animatePixels(e) {
         }
     });
     let timerId = setInterval(pixelate, 100);
+    changeChars(getNewContent(pageName), descriptionEl)
     setTimeout(() => {
         displayOneCorner();
         clearInterval(timerId);
-    }, 500);
+    }, 1080);
 }
